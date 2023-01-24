c81b212a932e627e9a9bba15a13cf3dd0ac41d53
min199973 <107176389+min199973@users.noreply.github.com>
2023-01-24 17:38:35 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 2;
+let myName = "mingoose";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "nicolas";
+
+console.log("your name " + myName);
